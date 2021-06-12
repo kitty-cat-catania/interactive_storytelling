48e84a53131b5fecc2ba41e644136b42e7c19e95
tmsteph <tmsteph1290@gmail.com>
2021-06-11 19:38:46 -0700

    created flask app.py 'hello world' route

--- code/app.py
+++ code/app.py
@@ -1,4 +1,23 @@
 ### Write Flask App Below 
 
 import sqlite3
-conn = sqlite3.connect("map.db")
+from flask import Flask  
+
+
+conn = sqlite3.connect("migration.db")
+c = conn.cursor()
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return "Hello World"
+	
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
