86f270b7b86f0ddaa4c58467f390a0754c0ae349
tmsteph <tmsteph1290@gmail.com>
2021-06-12 12:10:41 -0700

    Working prototype data export from Flask

--- code/app.py
+++ code/app.py
@@ -1,21 +1,50 @@
 ### Write Flask App Below 
-
+import json
 import sqlite3
-from flask import Flask  
-
-
-conn = sqlite3.connect("migration.db")
-c = conn.cursor()
-
-
-
-
+from flask import Flask
+from flask import render_template
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+
+
+
+
+@app.route('/list')
+def list():
+   con = sqlite3.connect("migration.db")
+   cur = con.cursor()
+   cur.execute("select * from trends")
+   rows = cur.fetchall()
+   desc = cur.description
+   data = []
+   for row in rows:
+      data.append({ desc[i][0]: v for (i,v) in enumerate(row) })
+    
+   return json.dumps(data) 
+   #render_template("list.html",rows = data)
+
+
+#database configuration
+
+
+# c.execute("SELECT * FROM a6_distance LIMIT 5")
+# print(c.fetchall())
+
+# c.execute("SELECT * FROM trends LIMIT 5")
+# print(c.fetchall())
+
+
+
+
 
 @app.route('/')
 def hello_world():
 	return "Hello World"
+
+
 	
 
 
