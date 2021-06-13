e040b9c3a9c7917795f7d92256a57f3bc1d19821
tmsteph <tmsteph1290@gmail.com>
2021-06-13 16:26:55 -0700

    working

--- code/static/js/animation.js
+++ code/static/js/animation.js
@@ -7,14 +7,14 @@ class Particle {
 	    this.x = random(0,width);
 	    this.y = random(0,height);
 	    this.r = random(1,8);
-	    this.xSpeed = random(-2,2);
-	    this.ySpeed = random(-1,1.5);
+	    this.xSpeed = random(-.01,.01);
+	    this.ySpeed = random(-.01,.01);
 	  }
 	
 	// creation of a particle.
 	  createParticle() {
 	    noStroke();
-	    fill('rgba(200,169,169,0.5)');
+	    fill('rgba(255,110,169,0.8)');
 	    circle(this.x,this.y,this.r);
 	  }
 	
@@ -34,7 +34,7 @@ class Particle {
 	    particles.forEach(element =>{
 	      let dis = dist(this.x,this.y,element.x,element.y);
 	      if(dis<85) {
-		stroke('rgba(255,255,255,0.04)');
+		stroke('rgba(255,255,255, .2)');
 		line(this.x,this.y,element.x,element.y);
 	      }
 	    });
@@ -45,17 +45,18 @@ class Particle {
 	let particles = [];
 	
 	function setup() {
-	  createCanvas(720, 400);
+	  select("#myCanvas");
 	  for(let i = 0;i<width/10;i++){
 	    particles.push(new Particle());
 	  }
 	}
 	
 	function draw() {
-	  background('#0f0f0f');
+	  background('#d0ffff');
 	  for(let i = 0;i<particles.length;i++) {
 	    particles[i].createParticle();
 	    particles[i].moveParticle();
 	    particles[i].joinParticles(particles.slice(i));
 	  }
 	}
+p5.select("#myCanvas").Particle
