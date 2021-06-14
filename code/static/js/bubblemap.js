4f23e3fe3954db1bd48c557008d7304d196bcebf
toothFairyDragon <gabrielle.catania@gmail.com>
2021-06-13 19:16:40 -0700

    made bar chart interactive

--- code/static/js/bubblemap.js
+++ code/static/js/bubblemap.js
@@ -35,8 +35,48 @@ d3.json ("/a1_movers").then(function (data) {
     var data = [trace1];
 
     var layout = {
-        title: 'Distance Moved'
+        title: 'Distance Moved',
+        yaxis: {
+            title: 'People (Thousands)'
+        }
     };
 
     Plotly.newPlot('bubblemap_vis', data, layout);
-})
+});
+
+function optionChanged() {
+    d3.json("/a1_movers").then(function (data) {
+        var ddMenu = d3.select("#time");
+        var selectedYear = ddMenu.property("value");
+        console.log(selectedYear);
+        var yearIndex = 0;
+        for (var i = 0; i<data.length; i++){
+            state = data[i];
+            if (state.mobility_period == selectedYear) {
+                console.log(state.mobility_period);
+                yearIndex = i;
+                break;
+            };
+        };
+        selectedDict = data[yearIndex];
+        console.log(selectedDict);
+        selectedVals = Object.values(selectedDict);
+        console.log(selectedVals);
+        var newState = selectedDict.movers_new_county_new_state;
+        var newCounty = selectedDict.movers_new_county_same_state;
+        var sameCounty = selectedDict.movers_same_county;
+        var trace = {
+            x: ['New State', 'New County', 'Same County'],
+            y: [newState, newCounty, sameCounty],
+            type: 'bar'
+        };
+        var layout = {
+            title: 'Distance Moved',
+            yaxis: {
+                title: 'People (Thousands)'
+            }
+        };
+        Plotly.newPlot('bubblemap_vis', [trace], layout);
+
+    });
+}
