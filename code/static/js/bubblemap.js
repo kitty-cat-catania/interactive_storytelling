96e8cc7f6de32e588ce9cfdcdffcb72672249619
toothFairyDragon <gabrielle.catania@gmail.com>
2021-06-13 18:32:07 -0700

    worked on bar chart and another drop down menu

--- code/static/js/bubblemap.js
+++ code/static/js/bubblemap.js
@@ -1,8 +1,42 @@
 d3.select("#bubblemap_vis").text("TESTING BUBBLEMAP")
-d3.json("/trends").then(function(data) {
-    console.log(data);
-});
 
-d3.json("/a6_distance").then(function(data) {
-    console.log(data);
-});
+
+function init () {
+    d3.json ("/a1_movers").then(function (mobilData) {
+        ddButton = d3.select("#time")
+        
+        var years = mobilData.forEach(function (year) {
+            var period = year.mobility_period;
+            var idOption = ddButton.append("option");
+            idOption.text(period);
+            idOption.attr("value", period);
+        });
+
+        
+    });
+
+
+}
+init();
+
+d3.selectAll("#time").on("change", optionChanged);
+
+d3.json ("/a1_movers").then(function (data) {
+    var firstDict = data[0];
+    var newState = firstDict.movers_new_county_new_state;
+    var newCounty = firstDict.movers_new_county_same_state;
+    var sameCounty = firstDict.movers_same_county;
+    var trace1 = {
+        x: ['New State', 'New County', 'Same County'],
+        y: [newState, newCounty, sameCounty],
+        type: 'bar'
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        title: 'Distance Moved'
+    };
+
+    Plotly.newPlot('bubblemap_vis', data, layout);
+})
