2320aae40861a739f9fe391cb3528088708523b6
tmsteph <tmsteph1290@gmail.com>
2021-06-15 15:17:27 -0700

    lightened background and added filler paragraphs

--- code/static/js/animation.js
+++ code/static/js/animation.js
@@ -6,7 +6,7 @@ class Particle {
 	  constructor(){
 	    this.x = random(0,width);
 	    this.y = random(0,height);
-	    this.r = random(2,2);
+	    this.r = random(1.5,1.5);
 	    this.xSpeed = random(-.04,.04);
 	    this.ySpeed = random(-.04,.04);
 	  }
@@ -14,7 +14,7 @@ class Particle {
 	// creation of a particle.
 	  createParticle() {
 	    noStroke();
-	    fill('rgba(255,255,255,1)');
+	    fill('rgba(230,255,255,.01');
 	    circle(this.x,this.y,this.r);
 	  }
 	
@@ -34,7 +34,7 @@ class Particle {
 	    particles.forEach(element =>{
 	      let dis = dist(this.x,this.y,element.x,element.y);
 	      if(dis<11) {
-		stroke('rgba(255,230,255, .8)');
+		stroke('rgba(255,230,255, .51)');
 		line(this.x,this.y,element.x,element.y);
 	      }
 	    });
@@ -52,7 +52,7 @@ class Particle {
 	}
 	
 	function draw() {
-	  background('rgba(215,255,255,1)');
+	  background('rgba(225,255,255,.2)');
 	  for(let i = 0;i<particles.length;i++) {
 	    particles[i].createParticle();
 	    particles[i].moveParticle();
